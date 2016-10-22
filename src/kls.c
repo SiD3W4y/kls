955bb862277c528ad5e4c5c1c8d80012a1326f2b
Sideway <dev.sideway@gmail.com>
2016-10-22 19:17:41 +0200

    Working PoC

--- src/kls.c
+++ src/kls.c
@@ -8,18 +8,58 @@
 #include <string.h>
 #include <time.h>
 
+
+const char *months[] = {
+	"jan.","feb.","mar.","apr."
+	,"may","jun.","jul.","aug.",
+	"sep.","oct.","nov.","dec."
+};
+
+
+
 void usage()
 {
 printf("kls Usage :\n");
 printf("kls <dir>\n");
 }
 
+void printmode(struct stat *st)
+{
+printf((S_ISDIR(st->st_mode)) ? "d" : "-");
+printf((st->st_mode & S_IRUSR) ? "r" : "-");
+printf((st->st_mode & S_IWUSR) ? "w" : "-");
+printf((st->st_mode & S_IXUSR) ? "x" : "-");
+printf((st->st_mode & S_IRGRP) ? "r" : "-");
+printf((st->st_mode & S_IWGRP) ? "w" : "-");
+printf((st->st_mode & S_IXGRP) ? "x" : "-");
+printf((st->st_mode & S_IROTH) ? "r" : "-");
+printf((st->st_mode & S_IWOTH) ? "w" : "-");
+printf((st->st_mode & S_IXOTH) ? "x" : "-");
+}
+
+void printdate(struct tm *time)
+{
+printf("%i %s %i:%i ",time->tm_mday,months[time->tm_mon],time->tm_hour,
+							time->tm_min);
+}
+
+
+
 void printentry(struct dirent *entry)
 {
-struct stat *dstat;
-stat(entry->d_name,dstat);
-printf("%s\n",entry->d_name);
+struct stat *dstat = malloc(sizeof(struct stat));
+struct tm *time;
 
+if(stat(entry->d_name,dstat) != 0){
+	perror("Stat error");
+}else{	
+	time = localtime(&dstat->st_mtime);
+	printmode(dstat);
+	printf(" %i ",(long)dstat->st_size);
+	printdate(time);
+	printf("%s\n",entry->d_name);
+}
+free(dstat);
 }
 
 
@@ -45,12 +85,12 @@ if(!entry){
 }
 
 while((dir = readdir(entry)) != NULL){
-	if(strcmp(".",dir->d_name)!=0 && strcmp("..",dir->d_name)!=0){
-		printentry(dir);	
+	if(strcmp(dir->d_name,".") != 0 && strcmp(dir->d_name,"..") !=0){
+		printentry(dir);
 	}
+
 }
 
-
-    
+return 0;
 }
     
